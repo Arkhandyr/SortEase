ba00c770711005ce755a27447f73df5b64fbc62d
Arkhandyr <victor.hm2000@aluno.ifsc.edu.br>
2023-04-15 00:20:42 -0300

    não lembro o que alterei...
    hehe

--- SortEase/SortService.cs
+++ SortEase/SortService.cs
@@ -11,14 +11,17 @@ namespace SortEase
             //mover os arquivos configurados para diretório X
             List<string> compressedExtensions = new() { ".zip", ".rar", ".7z" };
 
-            var dir = new DirectoryInfo("C:\\Users\\Arkhandyr\\Downloads");
+            var path = "C:\\Users\\Arkhandyr\\Downloads";
+
+            var dir = new DirectoryInfo(path);
             FileInfo[] files = dir.GetFiles();
+            var folders = Directory.GetDirectories(path);
 
             foreach (var file in files)
             {
                 string mimeType = MimeUtility.GetMimeMapping(file.FullName).Split("/")[0];
 
-                if (compressedExtensions.Contains(file.Extension)) //&& file.LastWriteTime > DateTime.Now.AddDays(-7))
+                if (compressedExtensions.Contains(file.Extension) && file.LastWriteTime > DateTime.Now.AddDays(-7))
                 {
                     file.Delete();
                     continue;
@@ -29,9 +32,17 @@ namespace SortEase
                 if (!Directory.Exists(newDir))
                     Directory.CreateDirectory(newDir);
 
-                File.Move(file.FullName, (Path.Combine(newDir, file.Name)));
+                File.Move(file.FullName, Path.Combine(newDir, file.Name));
+            }
 
-                
+            foreach (var folder in folders) 
+            {
+                var newDir = Path.Combine(dir.FullName, "folders");
+
+                if (!Directory.Exists("folders"))
+                    Directory.CreateDirectory("folders");
+
+                Directory.Move(folder, newDir);
             }
         }
     }
